92a59c465b5b95eaa89366ca346cf598a81e5ac2
AfsarHossain <afsar291722@gmail.com>
2021-10-10 23:40:47 +0600

    JavaScript added

---
+++ script.js
@@ -0,0 +1,38 @@
+let number1;
+let number2;
+
+number1 = Math.floor(Math.random() * 10 + 1);
+number2 = Math.floor(Math.random() * 10 + 1);
+
+document.getElementById("number-1").innerHTML = number1;
+document.getElementById("number-2").innerHTML = number2;
+
+let answer = number1 + number2;
+
+let checkAnswer = document.querySelector("input[type=text]");
+console.log(checkAnswer);
+let value = checkAnswer.value;
+
+let btn = document.querySelector("input[type=button][value=check]");
+
+btn.addEventListener("click", function () {
+  value = checkAnswer.value;
+  if (value == answer) {
+    alert("You are correct!!");
+  } else {
+    alert("You are incorrect, the answer was " + answer);
+  }
+
+  document.querySelector("input[type=text").value = "";
+
+  document.getElementById("number-1").innerHTML = "";
+  document.getElementById("number-2").innerHTML = "";
+
+  number1 = Math.floor(Math.random() * 10 + 1);
+  number2 = Math.floor(Math.random() * 10 + 1);
+
+  document.getElementById("number-1").innerHTML = number1;
+  document.getElementById("number-2").innerHTML = number2;
+
+  answer = number1 + number2;
+});
